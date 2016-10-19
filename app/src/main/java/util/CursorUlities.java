369db94c01ed2923094505019c80693b02e5b00a
RommiPlus <junqingt@gmail.com>
2016-10-19 17:41:23 +0800

    commit util method

---
+++ app/src/main/java/util/CursorUlities.java
@@ -0,0 +1,76 @@
+package util;
+
+import android.database.Cursor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import data.MovieContract.MovieDetailEntry;
+import data.MovieContract.MovieReviewsEntry;
+import data.MovieContract.MovieVideoEntry;
+import data.dao.MovieDetailInfo;
+import data.dao.Reviews;
+import data.dao.Videos;
+
+/**
+ * Created by 123 on 2016/10/19.
+ */
+
+public class CursorUlities {
+    public static MovieDetailInfo cursorToMovieDetailInfo(Cursor cursor) {
+        if (cursor.moveToFirst()) {
+            MovieDetailInfo info = new MovieDetailInfo();
+            info.setId(cursor.getInt(cursor.getColumnIndex(MovieDetailEntry._ID)));
+            info.setMovieId(cursor.getInt(cursor.getColumnIndex(MovieDetailEntry.COLUMN_MOVIE_ID)));
+            info.setRuntime(cursor.getInt(cursor.getColumnIndex(MovieDetailEntry.COLUMN_RUNTIME)));
+            info.setReleaseDate(cursor.getString(cursor.getColumnIndex(MovieDetailEntry.COLUMN_RELEASE_DATE)));
+            info.setPosterPath(cursor.getString(cursor.getColumnIndex(MovieDetailEntry.COLUMN_POSTER_PATH)));
+            info.setOverview(cursor.getString(cursor.getColumnIndex(MovieDetailEntry.COLUMN_OVERVIEW)));
+            info.setStar(cursor.getInt(cursor.getColumnIndex(MovieDetailEntry.COLUMN_STAR)));
+            info.setTitle(cursor.getString(cursor.getColumnIndex(MovieDetailEntry.COLUMN_TITLE)));
+            info.setVoteAverage(cursor.getFloat(cursor.getColumnIndex(MovieDetailEntry.COLUMN_VOTE_AVERAGE)));
+            return info;
+        }
+        return null;
+    }
+
+    public static List<Reviews> cursorToMovieReivews(Cursor cursor) {
+        List<Reviews> list = new ArrayList<>();
+        if (cursor.moveToFirst()) {
+            do {
+                Reviews info = new Reviews();
+                info.setId(cursor.getInt(cursor.getColumnIndex(MovieReviewsEntry._ID)));
+                info.setMovieId(cursor.getInt(cursor.getColumnIndex(MovieReviewsEntry.COLUMN_MOVIE_ID)));
+                info.setReviewId(cursor.getInt(cursor.getColumnIndex(MovieReviewsEntry.COLUMN_REVIEW_ID)));
+                info.setAuthor(cursor.getString(cursor.getColumnIndex(MovieReviewsEntry.COLUMN_AUTHOR)));
+                info.setContent(cursor.getString(cursor.getColumnIndex(MovieReviewsEntry.COLUMN_CONTENT)));
+                list.add(info);
+            } while (cursor.moveToNext());
+
+            return list;
+        }
+        return null;
+    }
+
+    public static List<Videos> cursorToMovieVideos(Cursor cursor) {
+        List<Videos> list = new ArrayList<>();
+        if (cursor.moveToFirst()) {
+            do {
+                Videos info = new Videos();
+                info.setId(cursor.getInt(cursor.getColumnIndex(MovieVideoEntry._ID)));
+                info.setMovieId(cursor.getInt(cursor.getColumnIndex(MovieVideoEntry.COLUMN_MOVIE_ID)));
+                info.setVideoId(cursor.getInt(cursor.getColumnIndex(MovieVideoEntry.COLUMN_VIDEO_ID)));
+                info.setKey(cursor.getString(cursor.getColumnIndex(MovieVideoEntry.COLUMN_KEY)));
+                info.setName(cursor.getString(cursor.getColumnIndex(MovieVideoEntry.COLUMN_NAME)));
+                list.add(info);
+            } while (cursor.moveToNext());
+
+            return list;
+        }
+        return null;
+    }
+
+
+
+
+}
---
+++ app/src/main/java/util/DetailMovieDataLoader.java
@@ -0,0 +1,128 @@
+package util;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.content.AsyncTaskLoader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import adapter.DetailAdapter.*;
+import adapter.DetailAdapter.ReviewHeader;
+import data.MovieContract.MovieDetailEntry;
+import data.MovieContract.MovieReviewsEntry;
+import data.MovieContract.MovieVideoEntry;
+import data.dao.MovieDetailInfo;
+import data.dao.Reviews;
+import data.dao.Videos;
+
+/**
+ * Created by 123 on 2016/10/19.
+ */
+
+public class DetailMovieDataLoader extends AsyncTaskLoader<List<Object>> {
+
+    final ForceLoadContentObserver mObserver;
+    private int mMovieId;
+
+    public DetailMovieDataLoader(Context context, int movieId) {
+        super(context);
+        mObserver = new ForceLoadContentObserver();
+        mMovieId = movieId;
+    }
+
+    @Override
+    public List<Object> loadInBackground() {
+        MovieDetailInfo info = CursorUlities.cursorToMovieDetailInfo(getMovieDetailCursor());
+        if (info == null) {
+            throw new IllegalArgumentException("Movie detail info should not be null");
+        }
+
+        List<Reviews> reviews = CursorUlities.cursorToMovieReivews(getMovieReviewsCursor());
+        List<Videos> videos = CursorUlities.cursorToMovieVideos(getMovieVideoCursor());
+
+        ArrayList<Object> list = new ArrayList<>();
+        list.add(info);
+        list.add(new TrailerHear());
+        if (videos != null) {
+            list.addAll(videos);
+        }
+
+        list.add(new ReviewHeader());
+        if (reviews != null) {
+            list.addAll(reviews);
+        }
+        return list;
+    }
+
+    @Override
+    public void deliverResult(List<Object> data) {
+        super.deliverResult(data);
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    protected void onStopLoading() {
+        cancelLoad();
+    }
+
+    @Override
+    protected void onReset() {
+        super.onReset();
+    }
+
+    public Cursor getMovieDetailCursor() {
+        Cursor cursor = getContext().getContentResolver()
+                .query(MovieDetailEntry.CONTENT_URI,
+                        null,
+                        MovieDetailEntry.COLUMN_MOVIE_ID + " = ?",
+                        new String[]{String.valueOf(mMovieId)},
+                        null);
+
+        registerContentObserver(cursor);
+        return cursor;
+    }
+
+    public Cursor getMovieReviewsCursor() {
+        Cursor cursor = getContext().getContentResolver()
+                .query(MovieReviewsEntry.CONTENT_URI,
+                        null,
+                        MovieReviewsEntry.COLUMN_MOVIE_ID + " = ?",
+                        new String[]{String.valueOf(mMovieId)},
+                        null);
+
+        registerContentObserver(cursor);
+        return cursor;
+    }
+
+    public Cursor getMovieVideoCursor() {
+        Cursor cursor = getContext().getContentResolver()
+                .query(MovieVideoEntry.CONTENT_URI,
+                        null,
+                        MovieVideoEntry.COLUMN_MOVIE_ID + " = ?",
+                        new String[]{String.valueOf(mMovieId)},
+                        null);
+
+        registerContentObserver(cursor);
+        return cursor;
+    }
+
+    public void registerContentObserver(Cursor cursor) {
+        if (cursor != null) {
+            try {
+                // Ensure the cursor window is filled.
+                cursor.getCount();
+                cursor.registerContentObserver(mObserver);
+            } catch (RuntimeException ex) {
+                cursor.close();
+                throw ex;
+            }
+        }
+    }
+
+
+}
